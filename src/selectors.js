cdcb86fd6730a9ae71e7193349e1e148f2735507
James Wills <jamesbwills@Jamess-MacBook-Pro.local>
2020-02-08 12:57:25 -0800

    prettify selectors file

--- src/selectors.js
+++ src/selectors.js
@@ -1,26 +1,28 @@
 import { createSelector } from 'reselect';
 import { rowPassesFilters } from './util/Filters';
 
-const getFilters = (state) => state.filters;
+const getFilters = state => state.filters;
 
-const getHousingRows = (state) => state.housingRows;
+const getHousingRows = state => state.housingRows;
 
-const getSortedColumn = (state) => state.sortedColumn;
+const getSortedColumn = state => state.sortedColumn;
 
-const getSortedAscending = (state) => state.sortedAscending;
+const getSortedAscending = state => state.sortedAscending;
 
 const getSortedRows = createSelector(
-  [ getHousingRows, getSortedColumn, getSortedAscending ],
+  [getHousingRows, getSortedColumn, getSortedAscending],
   (rows, sortedColumn, sortedAscending) => {
     if (!sortedColumn) {
       return rows;
     }
 
-    return [...rows].sort((a, b) => sortedColumn.comparator(a, b, sortedColumn, sortedAscending));
-  },
+    return [...rows].sort((a, b) =>
+      sortedColumn.comparator(a, b, sortedColumn, sortedAscending)
+    );
+  }
 );
 
 export const getFilteredSortedRows = createSelector(
-  [ getSortedRows, getFilters ],
+  [getSortedRows, getFilters],
   (rows, filters) => rows.filter(row => rowPassesFilters(filters, row))
 );
